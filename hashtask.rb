30c09179d4ba0df9622a4a1d89871b7ebeaaa2bc
KirilKostov <kiril_lol@abv.bg>
2013-05-27 01:16:06 +0300

    Changed identation to file hashtask.rb

--- hashtask.rb
+++ hashtask.rb
@@ -1,29 +1,18 @@
 class Hash
-def group_values
-result = {}
-each do |key, value|
-if result.has_key?(value)
-result[value] << (key)
-else
-result[value] = [key]
+  def group_values
+    result = {}
+    each do |key, value|
+      if result.has_key?(value)
+        result[value] << (key)
+      else
+        result[value] = [key]
+      end
+    end
+
+    result
+  end
+
+  def testit
+    puts 'it works'
+  end
 end
-end
-result
-end
-end
-
-
-
-=begin
-
- map method  {|key, value| value, key} 
-
-  result.has_key?(value) / (key) 
-
- ||= 
-
- invert method
-
- install pry?
-
- =end
