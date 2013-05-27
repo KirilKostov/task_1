4f2b8d4e046463a153eb7e5b604646a48de7fc03
KirilKostov <kiril_lol@abv.bg>
2013-05-27 16:31:37 +0300

    Add questions about the hashtask.rb file

--- hashtask.rb
+++ hashtask.rb
@@ -3,7 +3,7 @@ class Hash
     result = {}
     each do |key, value|
       if result.has_key?(value)
-        result[value] << (key)
+        result[value] << key
       else
         result[value] = [key]
       end
@@ -16,3 +16,20 @@ class Hash
     puts 'it works'
   end
 end
+
+
+
+
+=begin
+
+ map method  {|key, value| value, key} 
+
+  result.has_key?(value) / (key) 
+
+ ||= 
+
+ invert method
+
+ install pry?
+
+=end
